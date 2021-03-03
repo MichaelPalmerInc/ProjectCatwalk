ace70993745b42d8ee18090d46a5fd75b752a6fb
katerina-spb <katespb98@gmail.com>
2021-03-02 18:11:43 -0600

    Created redux store

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,18 @@
+import Redux from 'redux';
+import productReducer from './productReducer.js';
+
+var rootReducer = Redux.combineReducers({
+  productReducer
+});
+
+export default rootReducer;
+
+
+
+
+
+
+
+
+
+
