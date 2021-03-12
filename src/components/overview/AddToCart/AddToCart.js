e530656f1558ea6598ebfd1f800729fcb1ec4f4c
HKanabar <harshin.kanabar@gmail.com>
2021-03-12 15:05:13 -0600

    Changes to dropdown

--- src/components/overview/AddToCart/AddToCart.js
+++ src/components/overview/AddToCart/AddToCart.js
@@ -2,14 +2,29 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import { Grid } from '@material-ui/core';
+import {Grid,FormControl,InputLabel,Select,makeStyles } from '@material-ui/core';
 
 
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 const AddToCart = ({skus}) => {
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [anchorEl2, setAnchorEl2] = React.useState(null);
+  const [size, setSize] = React.useState('');
 
+  const classes = useStyles();
+
+  const handleChange = (event) => {
+    setSize(event.target.value);
+  };
   const handleClose = () => {
     setAnchorEl(null);
     setAnchorEl2(null);
@@ -18,27 +33,29 @@ const AddToCart = ({skus}) => {
   return (
     <div>
     <div>
+
     <Grid container direction="row" alignItems="center" alignContent="center" justify ="center" spacing = {1}>
     <Grid item xs = {6}>
-      <Button aria-controls="sizes" aria-haspopup="true" onClick={e => setAnchorEl(e.currentTarget)}>
-        Select Size
-      </Button>
-      <Menu
-        id="sizes"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        {Object.keys(skus).map(keyname => (
-          <MenuItem onClick={(e)=> {
-            console.log(e.target.value);
+    <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel id="size">Select Size</InputLabel>
+        <Select
+          labelId="size"
+          id="size"
+          value={size}
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>Select Size</em>
+          </MenuItem>
+         {Object.keys(skus).map(keyname => (
+          <MenuItem value = {skus[keyname].size} onClick={(e)=> {
             console.log(skus[keyname].size);
             handleClose();
           }}
            >{skus[keyname].size}</MenuItem>
         ))}
-      </Menu>
+        </Select>
+      </FormControl>
       </Grid>
       <Grid item xs = {6}>
       <Button aria-controls="quantity" aria-haspopup="true" onClick={e => setAnchorEl2(e.currentTarget)}>
